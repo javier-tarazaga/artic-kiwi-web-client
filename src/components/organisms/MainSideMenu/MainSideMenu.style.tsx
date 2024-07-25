2d51f5fb3905aae525bee40249f90f2bc8c853f3
Javier Tarazaga <javier-tarazaga@users.noreply.github.com>
2024-07-25 13:42:20 +0200

    Improved main side menu

--- src/components/organisms/MainSideMenu/MainSideMenu.style.tsx
+++ src/components/organisms/MainSideMenu/MainSideMenu.style.tsx
@@ -1,7 +1,65 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const MenuContainer = styled.div`
-  background-color: #383C3F;
+  background-color: #353339;
   padding: 15px;
   height: 100%;
-`
+  color: white;
+  font-family: Arial, sans-serif;
+`;
+
+export const MenuHeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+
+  & > div {
+    margin-left: 10px;
+    font-weight: bold;
+  }
+
+  .expand-icon {
+    margin-left: auto;
+    cursor: pointer;
+  }
+`;
+
+export const MenuItemContainer = styled.div<{ active?: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+  cursor: pointer;
+  color: ${props => (props.active ? '#fff' : '#b0b0b0')};
+
+  & > div {
+    margin-left: 10px;
+  }
+
+  &:hover {
+    background-color: #383C3F;
+  }
+`;
+
+export const WorkspaceHeader = styled.div`
+  margin-top: 20px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  color: #b0b0b0;
+`;
+
+export const WorkspaceItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+  cursor: pointer;
+  color: #b0b0b0;
+
+  & > div {
+    margin-left: 10px;
+  }
+
+  &:hover {
+    color: white;
+  }
+`;
--- src/components/organisms/MainSideMenu/MainSideMenu.tsx
+++ src/components/organisms/MainSideMenu/MainSideMenu.tsx
@@ -1,49 +1,56 @@
 import React from 'react';
-import { MenuContainer } from './MainSideMenu.style';
+import { MenuContainer, MenuHeaderContainer, MenuItemContainer, WorkspaceHeader, WorkspaceItemContainer } from './MainSideMenu.style';
 import Icon from 'components/atoms/Icon';
 
 interface MenuItemProps {
   title: string;
-  children?: any;
+  iconName?: string;
+  active?: boolean;
+}
+
+interface MenuHeaderProps {
+  title: string;
   iconName?: string;
 }
 
-function MenuItem({ iconName, title, children }: MenuItemProps) {
+const MenuHeader: React.FC<MenuHeaderProps> = ({ iconName, title }) => {
   return (
-    <div>
-      { iconName && <Icon name={iconName} /> }
+    <MenuHeaderContainer>
+      {iconName && <Icon name={iconName} />}
       <div>{title}</div>
-      { children }
-    </div>
-  )
-}
+      <Icon name="arrow-down" className="expand-icon" />
+    </MenuHeaderContainer>
+  );
+};
 
-function MenuHeader({ title }: MenuItemProps) {
+const MenuItem: React.FC<MenuItemProps> = ({ iconName, title, active }) => {
   return (
-    <div>
+    <MenuItemContainer active={active}>
+      {iconName && <Icon name={iconName} />}
       <div>{title}</div>
-    </div>
-  )
-}
+    </MenuItemContainer>
+  );
+};
 
-function MainSideMenu() {
+const WorkspaceItem: React.FC<MenuItemProps> = ({ iconName, title }) => {
+  return (
+    <WorkspaceItemContainer>
+      {iconName && <Icon name={iconName} />}
+      <div>{title}</div>
+    </WorkspaceItemContainer>
+  );
+};
+
+const MainSideMenu: React.FC = () => {
   return (
     <MenuContainer>
-      <div>
-        <div>Javier Tarazaga</div>
-        <div>Settings</div>
-      </div>
-      <MenuItem iconName="time" title="My Day" />
-      <MenuItem title="Lists">
-        <div>
-          <p>hola</p>
-        </div>
-      </MenuItem>
-      <div>
-        <p></p>
-      </div>
+      <MenuHeader iconName="chevron-down" title="Acme Corp" />
+      <WorkspaceHeader>Workspace</WorkspaceHeader>
+      <WorkspaceItem iconName="running" title="Acme '24 F/W" />
+      <WorkspaceItem iconName="camera" title="Team Photo Shoot" />
+      <WorkspaceItem iconName="book" title="Campaign Story" />
     </MenuContainer>
-  )
-}
+  );
+};
 
 export default MainSideMenu;
